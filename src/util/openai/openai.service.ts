985c43ad5adcb9df7c00d5dce17af39be9df3db4
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-20 12:06:09 +0900

    add :: openai

---
+++ src/util/openai/openai.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources';
+
+@Injectable()
+export class OpenaiService {
+  private readonly openai: OpenAI;
+
+  constructor() {
+    this.openai = new OpenAI({
+      project: process.env.OPENAI_PROJECT,
+      organization: process.env.OPENAI_ORGANIZATION,
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+  }
+
+  async sendGpt(
+    messages: Array<ChatCompletionMessageParam>,
+  ): Promise<OpenAI.Chat.Completions.ChatCompletion> {
+    const completion = await this.openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      max_tokens: 300,
+      messages: messages,
+    });
+
+    return completion;
+  }
+}
