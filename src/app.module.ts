2b21328c5ce048ec71b1b5ec92f217c957725a56
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-20 11:58:15 +0900

    add :: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { OyoModule } from './oyo/oyo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: "./.env"
+    }),
+    OyoModule,
+  ],
 })
 export class AppModule {}
